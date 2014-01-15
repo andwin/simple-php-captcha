b729c9a62c8c7938fd888512fda5e25b65021740
andwin <anders.windelhed@gmail.com>
2014-01-16 00:34:28 +0100

    Centering captcha text in image

--- generate-captcha.php
+++ generate-captcha.php
@@ -9,13 +9,21 @@ $fontPath = getFontPath();
 
 $imageWidth = 160;
 $imageHeight = 50;
+$fontSize = 30;
 $angle = rand(-5, 5);
 
 $image = imagecreatetruecolor($imageWidth, $imageHeight);
 $fontColor = getFontColor($image);
 $backgroundColor = imagecolorallocate($image, 255, 255, 255);
 imagefilledrectangle($image, 0, 0, $imageWidth, $imageHeight, $backgroundColor);
-imagettftext($image, 30, $angle, 10, 40, $fontColor, $fontPath, $captcha);
+
+$box = imagettfbbox($fontSize, $angle, $fontPath, $captcha);
+$textwidth = abs($box[4] - $box[0]);
+$x = ($imageWidth - $textwidth) / 2;
+$textHeight = abs($box[5] - $box[1]);
+$y = ($imageHeight + $textHeight) / 2 -5;
+
+imagettftext($image, $fontSize, $angle, $x, $y, $fontColor, $fontPath, $captcha);
 
 header("Content-type: image/png");
 imagepng($image);
